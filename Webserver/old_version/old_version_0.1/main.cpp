7de1ebc602ae8597dd1fc2a9b22ac1ff99570a3b
shenjl <selonsy@gmail.com>
2019-11-10 17:01:39 +0800

    update.

--- Webserver/old_version/old_version_0.1/main.cpp
+++ Webserver/old_version/old_version_0.1/main.cpp
@@ -30,42 +30,43 @@ const int TIMER_TIME_OUT = 500;
 
 
 extern pthread_mutex_t qlock;
-extern struct epoll_event *events;
-
+extern struct epoll_event* events;
 void acceptConnection(int listen_fd, int epoll_fd, const string &path);
 
-extern priority_queue<mytimer *, deque<mytimer *>, timerCmp> myTimerQueue;
+extern priority_queue<mytimer*, deque<mytimer*>, timerCmp> myTimerQueue;
 
-int socket_bind_listen(int port) {
+int socket_bind_listen(int port)
+{
     // 检查port值，取正确区间范围
     if (port < 1024 || port > 65535)
         return -1;
 
     // 创建socket(IPv4 + TCP)，返回监听描述符
     int listen_fd = 0;
-    if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    if((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
         return -1;
 
     // 消除bind时"Address already in use"错误
     int optval = 1;
-    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1)
+    if(setsockopt(listen_fd, SOL_SOCKET,  SO_REUSEADDR, &optval, sizeof(optval)) == -1)
         return -1;
 
     // 设置服务器IP和Port，和监听描述副绑定
     struct sockaddr_in server_addr;
-    bzero((char *) &server_addr, sizeof(server_addr));
+    bzero((char*)&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons((unsigned short) port);
-    if (bind(listen_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1)
+    server_addr.sin_port = htons((unsigned short)port);
+    if(bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
         return -1;
 
     // 开始监听，最大等待队列长为LISTENQ
-    if (listen(listen_fd, LISTENQ) == -1)
+    if(listen(listen_fd, LISTENQ) == -1)
         return -1;
 
     // 无效监听描述符
-    if (listen_fd == -1) {
+    if(listen_fd == -1)
+    {
         close(listen_fd);
         return -1;
     }
@@ -73,17 +74,20 @@ int socket_bind_listen(int port) {
     return listen_fd;
 }
 
-void myHandler(void *args) {
-    requestData *req_data = (requestData *) args;
+void myHandler(void *args)
+{
+    requestData *req_data = (requestData*)args;
     req_data->handleRequest();
 }
 
-void acceptConnection(int listen_fd, int epoll_fd, const string &path) {
+void acceptConnection(int listen_fd, int epoll_fd, const string &path)
+{
     struct sockaddr_in client_addr;
     memset(&client_addr, 0, sizeof(struct sockaddr_in));
     socklen_t client_addr_len = 0;
     int accept_fd = 0;
-    while ((accept_fd = accept(listen_fd, (struct sockaddr *) &client_addr, &client_addr_len)) > 0) {
+    while((accept_fd = accept(listen_fd, (struct sockaddr*)&client_addr, &client_addr_len)) > 0)
+    {
         /*
         // TCP的保活机制默认是关闭的
         int optval = 0;
@@ -91,10 +95,11 @@ void acceptConnection(int listen_fd, int epoll_fd, const string &path) {
         getsockopt(accept_fd, SOL_SOCKET,  SO_KEEPALIVE, &optval, &len_optval);
         cout << "optval ==" << optval << endl;
         */
-
+        
         // 设为非阻塞模式
         int ret = setSocketNonBlocking(accept_fd);
-        if (ret < 0) {
+        if (ret < 0)
+        {
             perror("Set non block failed!");
             return;
         }
@@ -103,7 +108,7 @@ void acceptConnection(int listen_fd, int epoll_fd, const string &path) {
 
         // 文件描述符可以读，边缘触发(Edge Triggered)模式，保证一个socket连接在任一时刻只被一个线程处理
         __uint32_t _epo_event = EPOLLIN | EPOLLET | EPOLLONESHOT;
-        epoll_add(epoll_fd, accept_fd, static_cast<void *>(req_info), _epo_event);
+        epoll_add(epoll_fd, accept_fd, static_cast<void*>(req_info), _epo_event);
         // 新增时间信息
         mytimer *mtimer = new mytimer(req_info, TIMER_TIME_OUT);
         req_info->addTimer(mtimer);
@@ -112,25 +117,29 @@ void acceptConnection(int listen_fd, int epoll_fd, const string &path) {
         pthread_mutex_unlock(&qlock);
     }
     //if(accept_fd == -1)
-    //   perror("accept");
+     //   perror("accept");
 }
-
 // 分发处理函数
-void handle_events(int epoll_fd, int listen_fd, struct epoll_event *events, int events_num, const string &path,
-                   threadpool_t *tp) {
-    for (int i = 0; i < events_num; i++) {
+void handle_events(int epoll_fd, int listen_fd, struct epoll_event* events, int events_num, const string &path, threadpool_t* tp)
+{
+    for(int i = 0; i < events_num; i++)
+    {
         // 获取有事件产生的描述符
-        requestData *request = (requestData *) (events[i].data.ptr);
+        requestData* request = (requestData*)(events[i].data.ptr);
         int fd = request->getFd();
 
         // 有事件发生的描述符为监听描述符
-        if (fd == listen_fd) {
+        if(fd == listen_fd)
+        {
             //cout << "This is listen_fd" << endl;
             acceptConnection(listen_fd, epoll_fd, path);
-        } else {
+        }
+        else
+        {
             // 排除错误事件
             if ((events[i].events & EPOLLERR) || (events[i].events & EPOLLHUP)
-                || (!(events[i].events & EPOLLIN))) {
+                || (!(events[i].events & EPOLLIN)))
+            {
                 printf("error event\n");
                 delete request;
                 continue;
@@ -155,45 +164,57 @@ void handle_events(int epoll_fd, int listen_fd, struct epoll_event *events, int 
 就不用再重新申请requestData节点了，这样可以继续重复利用前面的requestData，减少了一次delete和一次new的时间。
 */
 
-void handle_expired_event() {
+void handle_expired_event()
+{
     pthread_mutex_lock(&qlock);
-    while (!myTimerQueue.empty()) {
+    while (!myTimerQueue.empty())
+    {
         mytimer *ptimer_now = myTimerQueue.top();
-        if (ptimer_now->isDeleted()) {
+        if (ptimer_now->isDeleted())
+        {
             myTimerQueue.pop();
             delete ptimer_now;
-        } else if (ptimer_now->isvalid() == false) {
+        }
+        else if (ptimer_now->isvalid() == false)
+        {
             myTimerQueue.pop();
             delete ptimer_now;
-        } else {
+        }
+        else
+        {
             break;
         }
     }
     pthread_mutex_unlock(&qlock);
 }
 
-int main() {
+int main()
+{
     handle_for_sigpipe();
     int epoll_fd = epoll_init();
-    if (epoll_fd < 0) {
+    if (epoll_fd < 0)
+    {
         perror("epoll init failed");
         return 1;
     }
     threadpool_t *threadpool = threadpool_create(THREADPOOL_THREAD_NUM, QUEUE_SIZE, 0);
     int listen_fd = socket_bind_listen(PORT);
-    if (listen_fd < 0) {
+    if (listen_fd < 0) 
+    {
         perror("socket bind failed");
         return 1;
     }
-    if (setSocketNonBlocking(listen_fd) < 0) {
+    if (setSocketNonBlocking(listen_fd) < 0)
+    {
         perror("set socket non block failed");
         return 1;
     }
     __uint32_t event = EPOLLIN | EPOLLET;
     requestData *req = new requestData();
     req->setFd(listen_fd);
-    epoll_add(epoll_fd, listen_fd, static_cast<void *>(req), event);
-    while (true) {
+    epoll_add(epoll_fd, listen_fd, static_cast<void*>(req), event);
+    while (true)
+    {
         int events_num = my_epoll_wait(epoll_fd, events, MAXEVENTS, -1);
         //printf("%zu\n", myTimerQueue.size());        
         if (events_num == 0)
